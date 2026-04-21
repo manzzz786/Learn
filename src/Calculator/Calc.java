f19c2c4208f13d76be046458be5e88bc3e0c4f8d
manzzz786 <syedz.7pro@gmail.com>
2026-04-21 00:06:08 -0500

    To-do list.

---
+++ src/Calculator/Calc.java
@@ -0,0 +1,25 @@
+package Calculator;
+
+public class Calc {
+/*things to do 
+Create Methods for following
+Cacluclator program
+Method for calculating area of triangle
+Method for calculating area of circle
+Method for calculating area of rectnagle
+Method for calculating (a+b)2
+Bank Program
+Method for calculting simple interest
+Method for withdraw
+Method for deposit
+Ecommerce program
+Method for login
+Method for registration
+Method of add to cart
+Method for payment
+Mail Program
+Method for sending email
+Method for deleting email
+Method for read email
+*/
+}
